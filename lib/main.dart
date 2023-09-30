8fdc9cb1b0f33d742acd45c8412e6adb8a0b3358
Jose De avila <jrdeavila@unicesar.edu.co>
2023-09-30 13:38:23 -0500

    Infrastructura y Modelo de Desarrollo ONION

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:club_valledupar_app/lib.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      theme: ColorPalete.themeData,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
