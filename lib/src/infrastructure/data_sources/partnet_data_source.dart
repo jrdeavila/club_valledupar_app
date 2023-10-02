2762c84798b0dda1fe20e1f95a65af34fb0bb6bb
Jose De avila <jrdeavila@unicesar.edu.co>
2023-10-02 15:44:08 -0500

    Corregir bug de data source partner

--- lib/src/infrastructure/data_sources/partnet_data_source.dart
+++ lib/src/infrastructure/data_sources/partnet_data_source.dart
@@ -1,7 +1,7 @@
 import 'package:club_valledupar_app/lib.dart';
 
 Partner partnerFromJson(JSON json) => Partner(
-      id: json['id'],
+      id: json['id'].toString(),
       firstname: json['firstname'],
       lastname: json['lastname'],
       email: json['email'],
